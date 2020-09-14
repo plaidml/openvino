1641f1c58580cd9d2b179f4aa0632ba5de163bdd
Tim Zerrell <tim.zerrell@intel.com>
2020-09-14 11:53:18 -0700

    WIP Extra log on throw

--- inference-engine/src/readers/ir_reader/ie_ir_parser.cpp
+++ inference-engine/src/readers/ir_reader/ie_ir_parser.cpp
@@ -212,6 +212,7 @@ V10Parser::GenericLayerParams V10Parser::parseGenericParams(const pugi::xml_node
             const pugi::char_t* dimVal = node.child_value();
             std::stringstream ss(dimVal);
             if (!(ss >> dim) || dim == 0) {
+                std::cout << "ABOUT TO THROW\n";
                 THROW_IE_EXCEPTION << "dimension (" << dimVal << ") in node " << node.name()
                                    << " must be a positive integer: at offset " << node.offset_debug();
             }
