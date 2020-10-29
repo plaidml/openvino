c670ef86f70edb987011b093654f49fa8e14fad6
Mike Yi <michael.w.yi@intel.com>
2020-10-29 14:21:55 -0700

    variadic split w/o -1

---
+++ inference-engine/src/plaidml_plugin/ops/variadic_split.cpp
@@ -0,0 +1,52 @@
+// Copyright (C) 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include "plaidml_ops.hpp"
+#include "plaidml_util.hpp"
+
+#include "ngraph/opsets/opset.hpp"
+#include "ngraph/opsets/opset1.hpp"
+
+#include "plaidml/op/op.h"
+
+using namespace plaidml;          // NOLINT[build/namespaces]
+
+namespace PlaidMLPlugin {
+
+static OpRegistration reg("variadicsplit", [](const Context& ctx) {
+  IE_ASSERT(ctx.operands.size() == 3);
+  auto I = ctx.operands.at(0);
+  auto axes = get_axis_vector_from_constant_operand(1, ctx.layer);
+  IE_ASSERT(axes.size() == 1);
+  auto axis = axes[0];
+  auto split_lengths = get_shape_from_constant_operand(2, ctx.layer);
+
+  auto ndims = I.rank();
+  std::vector<edsl::TensorDim> I_dims(ndims);
+  std::vector<edsl::TensorIndex> I_idxs(ndims);
+  std::vector<edsl::Tensor> Os;
+  I.bind_dims(I_dims);
+  auto O_dims = I_dims;
+  size_t offset = 0;
+  size_t total_size = 0;
+  for (auto split : split_lengths) {
+      if (split > -1) {
+        total_size += split;
+      }
+  }
+  // size_t placeholder = I_dims[axis] - total_size;
+  for (auto split : split_lengths) {
+    // if (split == -1) {
+    //     split = placeholder;
+    // }
+    O_dims[axis] = static_cast<edsl::TensorDim>(split);
+    auto O_idxs = I_idxs;
+    O_idxs[axis] = I_idxs[axis] - offset;
+    offset += split;
+    Os.push_back(plaidml::edsl::Contraction().outShape(O_dims).outAccess(O_idxs).assign(I(I_idxs)));
+  }
+  return edsl::make_tuple(Os);
+});
+
+}  // namespace PlaidMLPlugin
