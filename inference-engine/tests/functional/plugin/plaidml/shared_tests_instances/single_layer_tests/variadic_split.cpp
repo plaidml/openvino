9b91499ffd2a196ad00c68bea0d7eefc28e7fc00
Michael Yi <michael.w.yi@intel.com>
2020-11-02 13:06:23 -0800

    Variadic Split (#91)
    * variadic split w/o -1
    
    * -1 implemented
    
    * Fix -1 support in variadic split and test it
    
    * Style consistency
    
    * Cleanup
    
    * Fix exception message

--- inference-engine/tests/functional/plugin/plaidml/shared_tests_instances/single_layer_tests/variadic_split.cpp
+++ inference-engine/tests/functional/plugin/plaidml/shared_tests_instances/single_layer_tests/variadic_split.cpp
@@ -13,7 +13,7 @@ namespace {
 
     const std::vector<InferenceEngine::Precision> netPrecisions = {
             InferenceEngine::Precision::FP32,
-            InferenceEngine::Precision::FP16
+            // InferenceEngine::Precision::FP16
     };
 
     // Sum of elements numSplits = inputShapes[Axis]
@@ -31,6 +31,6 @@ namespace {
             ::testing::Values(0, 1, 2, 3),
             ::testing::ValuesIn(netPrecisions),
             ::testing::Values(std::vector<size_t>({30, 30, 30, 30})),
-            ::testing::Values(CommonTestUtils::DEVICE_CPU)),
+            ::testing::Values(CommonTestUtils::DEVICE_PLAIDML)),
             VariadicSplitLayerTest::getTestCaseName);
 }  // namespace
