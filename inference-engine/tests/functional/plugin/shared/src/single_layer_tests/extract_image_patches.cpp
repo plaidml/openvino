ace69e2632e46f6c8d1da1eb45c458bb509c72ee
xinghong chen <xinghong.chen@intel.com>
2020-11-05 01:16:51 +0800

    Add extract image patches op (#96)

--- inference-engine/tests/functional/plugin/shared/src/single_layer_tests/extract_image_patches.cpp
+++ inference-engine/tests/functional/plugin/shared/src/single_layer_tests/extract_image_patches.cpp
@@ -14,44 +14,45 @@
 #include "functional_test_utils/layer_test_utils.hpp"
 #include "ngraph_functions/builders.hpp"
 
-
 namespace LayerTestsDefinitions {
 
 std::string ExtractImagePatchesTest::getTestCaseName(const testing::TestParamInfo<extractImagePatchesTuple> &obj) {
     std::vector<size_t> inputShape, kernel, strides, rates;
-    ngraph::op::PadType pad_type;
+    ngraph::op::PadType padType;
     InferenceEngine::Precision netPrc;
     std::string targetName;
-    std::tie(inputShape, kernel, strides, rates, pad_type, netPrc, targetName) = obj.param;
+    std::tie(inputShape, kernel, strides, rates, padType, netPrc, targetName) = obj.param;
     std::ostringstream result;
     result << "IS=" << CommonTestUtils::vec2str(inputShape) << "_";
     result << "netPRC=" << netPrc.name() << "_";
     result << "K=" << CommonTestUtils::vec2str(kernel) << "_";
     result << "S=" << CommonTestUtils::vec2str(strides) << "_";
     result << "R=" << CommonTestUtils::vec2str(rates) << "_";
-    result << "P=" << pad_type << "_";
+    result << "P=" << padType << "_";
     result << "targetDevice=" << targetName;
     return result.str();
 }
 
 void ExtractImagePatchesTest::SetUp() {
-    std::vector<size_t> inputShape, kernel, strides, rates;
-    ngraph::op::PadType pad_type;
-    InferenceEngine::Precision netPrecision;
-    std::tie(inputShape, kernel, strides, rates, pad_type, netPrecision, targetDevice) = this->GetParam();
-    auto ngPrc = FuncTestUtils::PrecisionUtils::convertIE2nGraphPrc(netPrecision);
-
-    auto inputNode = std::make_shared<ngraph::opset1::Parameter>(ngPrc, ngraph::Shape(inputShape));
-    ngraph::ParameterVector params = {inputNode};
-
-    auto extImgPatches = std::make_shared<ngraph::opset3::ExtractImagePatches>(
-        inputNode, ngraph::Shape(kernel), ngraph::Strides(strides), ngraph::Shape(rates), pad_type);
-    ngraph::ResultVector results{std::make_shared<ngraph::opset1::Result>(extImgPatches)};
-    function = std::make_shared<ngraph::Function>(results, params, "ExtractImagePatches");
+  std::vector<size_t> inputShape;
+  InferenceEngine::SizeVector kernel, strides, rates;
+  ngraph::op::PadType padType;
+  InferenceEngine::Precision netPrecision;
+  std::tie(inputShape, kernel, strides, rates, padType, netPrecision,
+           targetDevice) = this->GetParam();
+  auto ngPrc = FuncTestUtils::PrecisionUtils::convertIE2nGraphPrc(netPrecision);
+  auto paramsIn = ngraph::builder::makeParams(ngPrc, {inputShape});
+  auto paramOut = ngraph::helpers::convert2OutputVector(
+          ngraph::helpers::castOps2Nodes<ngraph::op::Parameter>(paramsIn));
+  auto extImgPatches = std::dynamic_pointer_cast<ngraph::opset3::ExtractImagePatches>(
+          std::make_shared<ngraph::opset3::ExtractImagePatches>(paramOut[0], ngraph::Shape(kernel),
+                                                            ngraph::Strides(strides), ngraph::Shape(rates), padType));
+  ngraph::ResultVector results{std::make_shared<ngraph::opset1::Result>(extImgPatches)};
+  function = std::make_shared<ngraph::Function>(results, paramsIn, "ExtractImagePatches");
 }
 
 TEST_P(ExtractImagePatchesTest, CompareWithRefs) {
     Run();
 };
 
-}  // namespace LayerTestsDefinitions
+} // namespace LayerTestsDefinitions
