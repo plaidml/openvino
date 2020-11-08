23ee4acc5b9b071651cf22cc9dd06aea87ab8ca2
Zhibin Li <zhibin.li@intel.com>
2020-11-09 01:42:12 +0800

    fix

--- inference-engine/src/plaidml_plugin/ops/embedding_bag_offsets_sum.cpp
+++ inference-engine/src/plaidml_plugin/ops/embedding_bag_offsets_sum.cpp
@@ -37,17 +37,19 @@ static OpRegistration reg("EmbeddingBagOffsetsSum", [](const Context& ctx) {
   IE_ASSERT(ctx.operands.size() <= 5);
   auto I = ctx.operands.at(0);
   auto indices = ctx.operands.at(1);
-  auto num_indices = cast_constant_operand<size_t>(1, layer).size();
+  IE_ASSERT(indices.rank() == 1);
+  auto num_indices = indices.compute_shape().sizes()[0];
   auto offsets = cast_constant_operand<int32_t>(2, layer);
   int32_t default_index = -1;
-  std::vector<float> per_sample_weights(num_indices, 1);
+  Tensor per_sample_weights;
+  bool with_weights = false;
 
   if (ctx.operands.size() >= 4) {
     auto default_index_vec = cast_constant_operand<int32_t>(3, layer);
     default_index = default_index_vec[0];
     if (ctx.operands.size() == 5) {
-      per_sample_weights.clear();
-      per_sample_weights = cast_constant_operand<float>(4, layer);
+      per_sample_weights = ctx.operands.at(4);
+      with_weights = true;
     }
   }
 
@@ -64,25 +66,38 @@ static OpRegistration reg("EmbeddingBagOffsetsSum", [](const Context& ctx) {
   auto O_dims = I_dims;
   auto O_idxs = I_idxs;
 
+  std::vector<TensorDim> w_dim(1);
+  std::vector<TensorIndex> w_idx(1);
+  auto wo_dim = w_dim;
+  auto wo_idx = w_idx;
+  if (with_weights == true) {
+    per_sample_weights.bind_dims(w_dim);
+  }
+
+  wo_dim[0] = edsl::TensorDim(1);
   O_dims[0] = edsl::TensorDim(1);
   for (size_t i = 0; i < num_indices; ++i) {
     O_idxs[0] = I_idxs[0] - i;
-    slices.push_back(edsl::Contraction(O_dims, O_idxs).sum(I_gathered(I_idxs)));
+    auto slice = edsl::Contraction(O_dims, O_idxs).sum(I_gathered(I_idxs)).build();
+    if (with_weights == true) {
+      wo_idx[0] = w_idx[0] - i;
+      slice = slice * edsl::Contraction(wo_dim, wo_idx).sum(per_sample_weights(w_idx)).build();
+    }
+    slices.push_back(slice);
   }
 
   for (uint32_t l = 0; l < batch; ++l) {
     if (offsets[l + 1] == offsets[l]) {
       if (default_index == -1) {
-        auto zero = cast(Tensor(0.0), slices[0].dtype());
-        Os.push_back(select(slices[0] > 0.0, zero, zero));
+        Os.push_back(slices[0] * 0);
       } else {
         O_idxs[0] = I_idxs[0] - default_index;
         Os.push_back(edsl::Contraction(O_dims, O_idxs).sum(I(I_idxs)));
       }
     } else {
-      Tensor t = slices[offsets[l]] * per_sample_weights[offsets[l]];
+      Tensor t = slices[offsets[l]];
       for (uint32_t i = offsets[l] + 1; i < offsets[l + 1]; ++i) {
-        t = t + slices[i] * per_sample_weights[i];
+        t = t + slices[i];
       }
       Os.push_back(t);
     }
