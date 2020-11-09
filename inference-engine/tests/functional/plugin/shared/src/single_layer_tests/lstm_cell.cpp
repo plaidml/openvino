91ff5889afbd194eb8f152267fc6361c09e0c546
Ling, Liyang <liyang.ling@intel.com>
2020-11-09 10:33:23 +0800

    Fix format

--- inference-engine/tests/functional/plugin/shared/src/single_layer_tests/lstm_cell.cpp
+++ inference-engine/tests/functional/plugin/shared/src/single_layer_tests/lstm_cell.cpp
@@ -4,16 +4,16 @@
 
 #include <tuple>
 #include <string>
-
+#include <vector>
 #include <memory>
 #include <functional>
 
 #include "ie_core.hpp"
-#include <vector>
 
 #include "common_test_utils/common_utils.hpp"
 #include "functional_test_utils/blob_utils.hpp"
 #include "functional_test_utils/precision_utils.hpp"
+#include "functional_test_utils/plugin_cache.hpp"
 #include "functional_test_utils/skip_tests_config.hpp"
 
 #include <transformations/lstm_cell_decomposition.hpp>
@@ -33,10 +33,10 @@ std::string LSTMCellTest::getTestCaseName(const testing::TestParamInfo<LSTMCellP
     InferenceEngine::Precision netPrecision;
     std::string targetDevice;
     std::tie(should_decompose, batch, hidden_size, input_size, activations, clip, netPrecision,
-             targetDevice) = obj.param;
+            targetDevice) = obj.param;
     std::vector<std::vector<size_t>> inputShapes = {
-        {{batch, input_size}, {batch, hidden_size}, {batch, hidden_size}, {4 * hidden_size, input_size},
-            {4 * hidden_size, hidden_size}, {4 * hidden_size}},
+            {{batch, input_size}, {batch, hidden_size}, {batch, hidden_size}, {4 * hidden_size, input_size},
+                 {4 * hidden_size, hidden_size}, {4 * hidden_size}},
     };
     std::ostringstream result;
     result << "decomposition" << should_decompose << "_";
@@ -64,14 +64,14 @@ void LSTMCellTest::SetUp() {
     std::tie(should_decompose, batch, hidden_size, input_size, activations, clip, netPrecision,
              targetDevice) = this->GetParam();
     std::vector<std::vector<size_t>> inputShapes = {
-        {{batch, input_size}, {batch, hidden_size}, {batch, hidden_size}, {4 * hidden_size, input_size},
-         {4 * hidden_size, hidden_size}, {4 * hidden_size}},
+            {{batch, input_size}, {batch, hidden_size}, {batch, hidden_size}, {4 * hidden_size, input_size},
+                 {4 * hidden_size, hidden_size}, {4 * hidden_size}},
     };
     auto ngPrc = FuncTestUtils::PrecisionUtils::convertIE2nGraphPrc(netPrecision);
     auto params = ngraph::builder::makeParams(ngPrc, {inputShapes[0], inputShapes[1], inputShapes[2]});
     std::vector<ngraph::Shape> WRB = {inputShapes[3], inputShapes[4], inputShapes[5]};
     auto lstm_cell = ngraph::builder::makeLSTM(ngraph::helpers::convert2OutputVector(ngraph::helpers::castOps2Nodes(params)),
-                                               WRB, hidden_size, activations, {}, {}, clip);
+            WRB, hidden_size, activations, {}, {}, clip);
     ngraph::ResultVector results{std::make_shared<ngraph::opset1::Result>(lstm_cell->output(0)),
                                  std::make_shared<ngraph::opset1::Result>(lstm_cell->output(1))};
     function = std::make_shared<ngraph::Function>(results, params, "lstm_cell");
@@ -81,6 +81,7 @@ void LSTMCellTest::SetUp() {
         m.run_passes(function);
     }
 }
+
 
 TEST_P(LSTMCellTest, CompareWithRefs) {
     Run();
