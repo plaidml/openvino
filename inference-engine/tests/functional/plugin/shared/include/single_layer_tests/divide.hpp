21a8f6fa5e793fff25fc10cdaae1143b33d6772b
nchoudhu <namrata.choudhury@intel.com>
2020-11-11 13:33:21 -0800

    Add tests for divide

---
+++ inference-engine/tests/functional/plugin/shared/include/single_layer_tests/divide.hpp
@@ -0,0 +1,34 @@
+// Copyright (C) 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <tuple>
+#include <string>
+#include <vector>
+#include <memory>
+
+#include "functional_test_utils/layer_test_utils.hpp"
+#include "ngraph_functions/builders.hpp"
+
+
+typedef std::tuple<
+    InferenceEngine::Precision,     // Net precision
+    std::vector<std::vector<size_t>>,   // Input shapes
+    std::string                     // Target device name
+> divideParams;
+
+namespace LayerTestsDefinitions {
+
+class DivideLayerTest : public testing::WithParamInterface<divideParams>,
+                        virtual public LayerTestsUtils::LayerTestsCommon {
+public:
+    static std::string getTestCaseName(testing::TestParamInfo<divideParams> obj);
+
+protected:
+    void SetUp() override;
+};
+
+}  // namespace LayerTestsDefinitions
+
---
+++ inference-engine/tests/functional/plugin/shared/src/single_layer_tests/divide.cpp
@@ -0,0 +1,58 @@
+// Copyright (C) 2020 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#include <tuple>
+#include <string>
+#include <vector>
+#include <memory>
+#include <functional>
+#include <functional_test_utils/skip_tests_config.hpp>
+
+#include "ie_core.hpp"
+#include "ngraph_functions/utils/ngraph_helpers.hpp"
+
+#include "common_test_utils/common_utils.hpp"
+#include "functional_test_utils/precision_utils.hpp"
+#include "functional_test_utils/blob_utils.hpp"
+#include "functional_test_utils/plugin_cache.hpp"
+#include "functional_test_utils/layer_test_utils.hpp"
+
+#include "single_layer_tests/divide.hpp"
+
+namespace LayerTestsDefinitions {
+
+std::string DivideLayerTest::getTestCaseName(testing::TestParamInfo<divideParams> obj) {
+    InferenceEngine::Precision netPrecision;
+    std::vector<std::vector<size_t>> inputShapes;
+    std::string targetDevice;
+    std::tie(netPrecision, inputShapes, targetDevice) = obj.param;
+    std::ostringstream result;
+    result << "IS_" << CommonTestUtils::vec2str(inputShapes) << "_";
+    result << "netPRC_" << netPrecision.name() << "_";
+    result << "targetDevice_" << targetDevice;
+    return result.str();
+}
+
+void DivideLayerTest::SetUp() {
+    std::vector<InferenceEngine::SizeVector> inputShapes;
+    InferenceEngine::Precision netPrecision;
+    std::tie(netPrecision, inputShapes, targetDevice) = this->GetParam();
+    auto ngPrc = FuncTestUtils::PrecisionUtils::convertIE2nGraphPrc(netPrecision);
+
+    auto paramsIn = ngraph::builder::makeParams(ngPrc, {inputShapes});
+    auto paramOuts = ngraph::helpers::convert2OutputVector(
+            ngraph::helpers::castOps2Nodes<ngraph::op::Parameter>(paramsIn));
+    IE_ASSERT(paramOuts.size() == 2);
+    const auto divide = std::make_shared<ngraph::opset1::Divide>(paramOuts.at(0), paramOuts.at(1));
+
+    ngraph::ResultVector results;
+    results.push_back(std::make_shared<ngraph::opset1::Result>(divide));
+    function = std::make_shared<ngraph::Function>(results, paramsIn, "divide");
+}
+
+TEST_P(DivideLayerTest, CompareWithRefs) {
+    Run();
+};
+
+}  // namespace LayerTestsDefinitions
