f07f4ba9f79445bbcd03a6347e1a78c59b893bdb
zhangding <ding.zhang@intel.com>
2021-02-02 04:44:31 -0500

    write deformableconvolution

---
+++ inference-engine/tests/functional/shared_test_classes/include/shared_test_classes/single_layer/deformable_convolution.hpp
@@ -0,0 +1,48 @@
+// Copyright (C) 2019 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
+
+#pragma once
+
+#include <tuple>
+#include <vector>
+#include <string>
+#include <memory>
+
+#include "shared_test_classes/base/layer_test_utils.hpp"
+#include "ngraph_functions/builders.hpp"
+#include "ngraph_functions/utils/ngraph_helpers.hpp"
+
+// ! [test_deformableConvolution:definition]
+typedef std::tuple<
+        InferenceEngine::SizeVector,    // Kernel size
+        InferenceEngine::SizeVector,    // Strides
+        std::vector<ptrdiff_t>,         // Pad begin
+        std::vector<ptrdiff_t>,         // Pad end
+        InferenceEngine::SizeVector,    // Dilation
+        size_t,                         // Num out channel
+	size_t,                         // Group
+	size_t,                         // Deformable group
+	ngraph::op::PadType             // Padding type
+> deformableConvSpecificParams;
+typedef std::tuple<
+        deformableConvSpecificParams,
+        InferenceEngine::Precision,     // Net precision
+        InferenceEngine::SizeVector,    // Input shapes
+        InferenceEngine::SizeVector,    // Deformable shape
+        LayerTestsUtils::TargetDevice   // Device name
+> deformableConvLayerTestParamsSet;
+namespace LayerTestsDefinitions {
+
+
+class DeformableConvolutionLayerTest : public testing::WithParamInterface<deformableConvLayerTestParamsSet>,
+                             virtual public LayerTestsUtils::LayerTestsCommon {
+public:
+    static std::string getTestCaseName(testing::TestParamInfo<deformableConvLayerTestParamsSet> obj);
+
+protected:
+    void SetUp() override;
+};
+// ! [test_deformableConvolution:definition]
+
+}  // namespace LayerTestsDefinitions
