dea5b0d00ace4d12259a83b5bd67b12f063b4194
zhangding <ding.zhang@intel.com>
2021-03-01 10:15:40 -0500

    fix style

--- inference-engine/tests/functional/shared_test_classes/include/shared_test_classes/single_layer/deformable_convolution.hpp
+++ inference-engine/tests/functional/shared_test_classes/include/shared_test_classes/single_layer/deformable_convolution.hpp
@@ -13,25 +13,25 @@
 #include "ngraph_functions/utils/ngraph_helpers.hpp"
 #include "shared_test_classes/base/layer_test_utils.hpp"
 
-typedef std::tuple<InferenceEngine::SizeVector,  // Kernel size
-                   InferenceEngine::SizeVector,  // Strides
-                   std::vector<ptrdiff_t>,       // Pad begin
-                   std::vector<ptrdiff_t>,       // Pad end
-                   InferenceEngine::SizeVector,  // Dilation
-                   size_t,                       // Num out channel
-                   size_t,                       // Group
-                   size_t,                       // Deformable group
-                   ngraph::op::PadType           // Padding type
-                   >
-    deformableConvSpecificParams;
-typedef std::tuple<deformableConvSpecificParams,
-                   InferenceEngine::Precision,    // Net precision
-                   InferenceEngine::SizeVector,   // Input shapes
-                   InferenceEngine::SizeVector,   // Deformable shape
-                   LayerTestsUtils::TargetDevice  // Device name
-                   >
-    deformableConvLayerTestParamsSet;
 namespace LayerTestsDefinitions {
+typedef std::tuple<
+	InferenceEngine::SizeVector,  // Kernel size
+        InferenceEngine::SizeVector,  // Strides
+        std::vector<int64_t>,         // Pad begin
+        std::vector<int64_t>,         // Pad end
+        InferenceEngine::SizeVector,  // Dilation
+        size_t,                       // Num out channel
+        size_t,                       // Group
+        size_t,                       // Deformable group
+        ngraph::op::PadType           // Padding type
+> deformableConvSpecificParams;
+typedef std::tuple<
+	deformableConvSpecificParams,
+        InferenceEngine::Precision,    // Net precision
+        InferenceEngine::SizeVector,   // Input shapes
+        InferenceEngine::SizeVector,   // Deformable shape
+        LayerTestsUtils::TargetDevice  // Device name
+> deformableConvLayerTestParamsSet;
 
 class DeformableConvolutionLayerTest : public testing::WithParamInterface<deformableConvLayerTestParamsSet>,
                                        virtual public LayerTestsUtils::LayerTestsCommon {
